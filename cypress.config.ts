2661e3516ba0d22e76424c9c9540070d296757c4
Raul Tifrea <raul.tifrea@siemens.com>
2022-09-20 13:23:24 +0300

    updated to Cypress 10

---
+++ cypress.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 10000,
+  pageLoadTimeout: 30000,
+  videosFolder: 'cypress/videos/testExecutionVideos',
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  video: false,
+  retries: {
+    runMode: 0,
+    openMode: 0,
+  },
+  env: {
+    webdriveruni_homepage: 'https://webdriveruniversity.com',
+    first_name: 'raul',
+  },
+  projectId: '7yjoqm',
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json',
+  },
+  experimentalStudio: true,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'https://webdriveruniversity.com',
+    excludeSpecPattern: ['*.js'],
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
