457dff56eba8e7c8dad1b20aa592d4e7154f2baa
raultifrea <raultifrea@gmail.com>
2023-12-27 12:17:06 +0100

    added local reports after test run

--- cypress.config.ts
+++ cypress.config.ts
@@ -22,8 +22,6 @@ export default defineConfig({
   reporterOptions: {
   },
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on)
       return require("./cypress/plugins/index.js")(on, config)
